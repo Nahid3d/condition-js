26aa93c18e7cb6493a625ea61571db4512a0977c
Nahid islam <webdevlopernahid@gmail.com>
2026-07-03 18:40:37 +0600

    js condition task compllite

---
+++ last-task.js
@@ -0,0 +1,64 @@
+let money = 700;
+if(money >= 500){
+    // console.log('you got a free coke');
+
+}else{
+    console.log('fee : 30 taka ');
+}
+
+
+// let bmi = 10;
+// if(bmi < 18.5){
+//     console.log('underweight');
+// }
+// else if(bmi >=18.5 && bmi <=24.9){
+//     console.log('normal weight');
+// }
+// else if(bmi >=25 && bmi <=29.9){
+//     console.log('overweight');
+// }
+// else{
+//     console.log('obese');
+
+// }
+
+// let weight = 55;
+// let height = 1.75;
+
+
+// let bmi = weight / (height * height);
+
+// console.log('Your BMi :' + bmi.toFixed(2));
+
+// if(bmi < 18.5){
+//     console.log('underweight');
+// }else if(bmi >=18.5 && bmi <= 24.9){
+//     console.log('normal weight');
+// }else if(bmi >=25 && bmi <= 29.9){
+//     console.log('overweight');
+// }else{
+//     console.log('obese');
+// }
+
+
+
+// grade calculator 
+
+let marks = 79;
+
+if(marks >= 90 && marks <= 100){
+    console.log('A+');
+}
+else if(marks >= 80 && marks <= 89){
+    console.log('B');
+}
+else if(marks >= 70 && marks <= 79){
+    console.log('C');
+}
+else if(marks >= 60 && marks <= 69){
+    console.log('D')
+
+}
+else{
+    console.log('F');
+};
